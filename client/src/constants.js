53ec85bb27d3dceee732a0017c39238da69a3c30
Eden-Ismah-Moshe <eden12012@gmail.com>
2024-09-18 16:05:48 +0300

    update api base url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 //export const API_BASE_URL = "http://10.100.102.10:5000";
-export const API_BASE_URL = "https://talku-talku-v3-server.vercel.app";
+export const API_BASE_URL = "https://student-scholar-shop-server.vercel.app";
 
 export const heroImages = [
   { imgUrl: "%PUBLIC_URL%/assets/hero-1.svg", alt: "watch" },
